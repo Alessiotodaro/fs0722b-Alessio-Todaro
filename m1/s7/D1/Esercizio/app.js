cd1aae8c58cdfba9e937cdf73718a39e57a30979
Alessio Todaro <alessiotodaro.palermo@gmail.com>
2022-12-06 12:33:58 +0100

    JS => chiamate ajax, jason

--- m1/s7/D1/Esercizio/app.js
+++ m1/s7/D1/Esercizio/app.js
@@ -6,23 +6,12 @@ function Persona(){
 }
 let onSub = document.querySelector('.btn').addEventListener('click', function(){
     let nuovaPersona = new Persona();
-    let prova = document.querySelector('.data').value
+    nuovaPersona.data = document.querySelector('.data').value
     nuovaPersona.nome = document.querySelector('.nome').value;
     nuovaPersona.cognome = document.querySelector('.cognome').value;
-    nuovaPersona.data = prova
-
-    function firstFunction (prova) {
-        let timestamp = Date.parse(prova);
-        let today = new Date().getTime();
-        let diff = ( today - timestamp ) / ( 1000 * 60 * 60 * 24 * 365 );
-        let n = parseInt( diff, 10 );
-        return n;
-    };
-    let test = firstFunction()
-    console.log(test)
     function newTable(){
         let table = document.querySelector('.tab')
-        table.innerHTML = `<tr><td> ${nuovaPersona.nome}</td><td>${nuovaPersona.cognome}</td><td>${test}</td></tr>`
+        table.innerHTML = `<tr><td> ${nuovaPersona.nome}</td><td>${nuovaPersona.cognome}</td><td>${nuovaPersona.data}</td></tr>`
         table.append
     }
     newTable()
