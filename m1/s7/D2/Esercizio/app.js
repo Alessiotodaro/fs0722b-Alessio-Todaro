cc2335e772efaaa02b5843fd0d61940b2f4a5355
Alessio Todaro <alessiotodaro.palermo@gmail.com>
2022-12-06 16:03:21 +0100

    Esercizio  => fine JS

--- m1/s7/D2/Esercizio/app.js
+++ m1/s7/D2/Esercizio/app.js
@@ -5,4 +5,17 @@ class Person {
         this.name = name;
         this.age = age;
     }
+    older(){
+        if(antoninoCannavacciuolo.age < brunoBarieri.age){
+            console.log(`${antoninoCannavacciuolo.name} è più giovane di ${brunoBarieri.name}`) 
+        }else{
+            console.log(`${antoninoCannavacciuolo.name} è più vecchio di ${brunoBarieri.name}`) 
+
+        }
+    }
 }
+const antoninoCannavacciuolo = new Person("Antonino", 43);
+const brunoBarieri = new Person("Bruno",60);
+
+console.log(brunoBarieri.older())
+console.log(antoninoCannavacciuolo.older())
