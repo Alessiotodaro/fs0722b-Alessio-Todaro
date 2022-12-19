509137add3d5fe44099a155b7d5bf157d407e434
Alessio Todaro <alessiotodaro.palermo@gmail.com>
2022-12-19 16:07:07 +0100

    Esercizio => Fine impostazioni TypeScript

--- m1/s9/intro.js
+++ m1/s9/intro.js
@@ -1,5 +1,17 @@
-var num = 12345;
-console.log(num);
-var message = "Hello World";
-console.log(message);
-var last = 12342344;
+function start() {
+    var inputValueY = document.getElementById("input");
+    var inputValueX = document.getElementById("input2");
+    var y = Number(inputValueY.value);
+    var x = Number(inputValueX.value);
+    var randomNumber = Math.floor(Math.random() * 100 + 1);
+    var ins = document.querySelector(".inserisciTesto");
+    if (y == randomNumber) {
+        ins.innerHTML = "Il ".concat(y, " \u00E8 uguale al numero casuale");
+    }
+    else if (x == randomNumber) {
+        ins.innerHTML = "Il ".concat(x, " \u00E8 uguale al numero casuale");
+    }
+    else {
+        ins.innerHTML = "I numeri inseriti non sono uguali al numero random. Numero Random = ".concat(randomNumber);
+    }
+}
--- m1/s9/intro.ts
+++ m1/s9/intro.ts
@@ -1,5 +1,19 @@
-var num:number = 12345
-console.log(num)
-var message:string = "Hello World"
-console.log(message)
-var last:number = 12342344
+function start(){
+    var inputValueY = (<HTMLInputElement>document.getElementById("input") as HTMLInputElement);
+    var inputValueX = (<HTMLInputElement>document.getElementById("input2") as HTMLInputElement);
+
+    var y = Number(inputValueY.value)
+    var x = Number (inputValueX.value)
+
+    let randomNumber:number = Math.floor(Math.random() * 100 + 1);
+    const ins: HTMLElement = document.querySelector(".inserisciTesto") as HTMLElement;
+
+    if(y == randomNumber){
+        ins.innerHTML =  `Il ${y} è uguale al numero casuale`
+    }else if(x == randomNumber){
+        ins.innerHTML =  `Il ${x} è uguale al numero casuale`
+    }else {
+        ins.innerHTML = `I numeri inseriti non sono uguali al numero random. Numero Random = ${randomNumber}`
+    }
+}
+
