8b5f7303efecdad4d92085ec9ae5a055f344bc16
Alessio Todaro <alessiotodaro.palermo@gmail.com>
2022-12-19 11:03:42 +0100

    TypeScript => Watch

---
+++ m1/s9/intro.js
@@ -0,0 +1,5 @@
+var num = 12345;
+console.log(num);
+var message = "Hello World";
+console.log(message);
+var last = 12342344;
---
+++ m1/s9/intro.ts
@@ -0,0 +1,5 @@
+var num:number = 12345
+console.log(num)
+var message:string = "Hello World"
+console.log(message)
+var last:number = 12342344
