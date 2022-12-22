c303632ded9791f269b8dcc83937b73c385b015d
Alessio Todaro <alessiotodaro.palermo@gmail.com>
2022-12-22 15:35:16 +0100

    Esercizio => typescript fetch

---
+++ m1/s9/D4/app.ts
@@ -0,0 +1,15 @@
+
+fetch(`./json/Abbigliamento.json`)
+      .then(res => res.json())
+      .then(res => {
+       let result = res[0]
+      })
+
+class CapoAbbiglimento {
+    capo: string
+    price: number
+    constructor(capo:string, price:number){
+        this.capo = res[0]
+    }
+}
+  
