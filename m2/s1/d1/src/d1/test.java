ee26616e79cbb36c4b279d5582eeffb45eab9cfc
Alessio Todaro <alessiotodaro.palermo@gmail.com>
2023-01-30 21:27:55 +0100

    Esercizio primo giorno Java

---
+++ m2/s1/d1/src/d1/test.java
@@ -0,0 +1,56 @@
+package d1;
+
+import java.util.Scanner;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* Esercizio 1
+		System.out.println("Moltiplica due numeri");
+		System.out.println("Inserisci primo numero");
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		System.out.println("Inserisci secondo numero");
+		int num2 = sc.nextInt();
+		int molt = num1 * num2;
+		System.out.println("Il risultato è " + molt );
+		*/
+		
+		/* Esercizio 2
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Inserisci una stringa");
+		String str = sc.nextLine();
+		System.out.println("Inserisci un numero intero");
+		int num = sc.nextInt();
+		System.out.println("Il risultato è: " + str + " " + num);
+		*/
+		
+		/* Esercizio 3
+		String [] stagioni;
+		stagioni = new String[4];
+		stagioni[0] = "Primavera";
+		stagioni[1] = "Inverno";
+		stagioni[2] = "Estate";
+		stagioni[3] = "Autunno";
+		int i = 0;
+		while(i < stagioni.length) {
+			System.out.println(stagioni[i]);
+			i++;
+		}
+		*/
+		
+		/* Prova
+		for (int i = 0; i > stagioni.length;i++) {
+			System.out.println(stagioni[i]);
+			};
+		*/
+		
+		/* Prova
+		for (int i = 0; i > stagioni.length; i++) {
+			System.out.println(stagioni [i]);
+		}*/
+		
+	}
+
+}
